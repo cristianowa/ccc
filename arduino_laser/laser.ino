2b5d03c10e9c7c95a17770b334427e6150cad4ac
Cristiano Araujo <cristianowerneraraujo@gmail.com>
2013-06-11 23:57:53 -0300

    Arduino laser control

---
+++ arduino_laser/laser.ino
@@ -0,0 +1,13 @@
+int laserPin = 3;
+void setup()
+{
+}
+void loop()
+{
+    /* top value is 170 3V3/5V = 0.66 => 255*0.66 ~= 170 */
+     
+   analogWrite(laserPin,168); 
+   delay(200);
+   analogWrite(laserPin,0); 
+   delay(200);
+}
