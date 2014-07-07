c6b58b4196a7b9ee471ac203b528936abdba8c12
Cristiano W. Araújo <cristianowerneraraujo@gmail.com>
2014-07-07 16:39:00 -0300

    Adding python-pptx revision and data set.

--- python_pptx/createSlides.py
+++ python_pptx/createSlides.py
@@ -19,31 +19,26 @@ def add_bullet_slide(presentation,title,bullets_text):
 	for i in bullets_text:
 		paragraph.text = i
 		paragraph = tf.add_paragraph()
-#	tf.text = 'Find the bullet slide layout'
 
+#read the source questions 
 source = open(sys.argv[1],"r")
-
 lines = source.readlines()
 title_slide(prs,lines[0],lines[1])
 lines = lines[2:len(lines)]
+#remove the line breaks
 for i in range(len(lines)):
 	lines[i] = lines[i].replace("\n","")
-	print lines[i]
-print lines
+#create questions from lines
 questions = []
 for l in range(len(lines)/5):
 	questions.append((lines[l*5],lines[l*5+1:l*5+5]))
-#	print " >>>> " +  lines[l*5]
-#	for i in lines[l*5+1:l*5+5]:
-#		print i
+#randomize questions
 random.shuffle(questions)
 for q in questions:
 	right = q[1][0]
+	#randomize answers
 	random.shuffle(q[1])
-	for i in range(len(q[1])):
-		if q[1][i] == right:
-			print right + " is at " + str(i+1)
-#	print q[1]
 	add_bullet_slide(prs,q[0],q[1])
 
-prs.save('test.pptx')
+#save presentation
+prs.save(sys.argv[2])
